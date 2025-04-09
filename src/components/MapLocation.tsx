1ccba0bfbc6a96af48f433c4a50afcf57ef4ce34
Federico Michel <fedesmi@gmail.com>
2025-04-09 11:41:51 -0300

    mapa

--- src/components/MapLocation.tsx
+++ src/components/MapLocation.tsx
@@ -7,13 +7,7 @@ const MapLocation: React.FC = () => {
       <h2 className="text-xl font-pixel text-center text-white mb-4">Mapa del Tesoro</h2>
       
       <div className="aspect-video w-full rounded overflow-hidden">
-        <iframe 
-          title="Ubicación de la fiesta" 
-          className="w-full h-full border-0"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.0167133965546!2d-58.4619872590354!3d-34.60378947545204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca0c687ee1e9%3A0x8e27a8c57c2c8699!2sJos%C3%A9%20A.%20Finocchio%201165%2C%20Buenos%20Aires%2C%20Argentina!5e0!3m2!1sen!2sus!4v1717726132185!5m2!1sen!2sus" 
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2357.2881237145766!2d-67.7178988!3d-53.78435929999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbc4b166ed52c94c7%3A0x9a1362cde9312018!2sJos%C3%A9%20A.%20Finocchio%201165%2C%20V9420%20R%C3%ADo%20Grande%2C%20Tierra%20del%20Fuego!5e0!3m2!1ses-419!2sar!4v1744209680465!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   );
